b2e56c18440258b09ae645c899f1f0cd1e106120
RamyashreeS11 <>
2021-05-26 12:29:13 +0000

    Problem_4.c

--- Problem_4.c
+++ Problem_4.c
@@ -5,12 +5,18 @@
 
 int main()
 {
-  	int n;
-
-  	scanf("%d", &n);
-
-  	//Write your code here
-  	
+  	int n,i,j;
+    printf("Enter the value of n\n");
+    scanf("%d", &n);
+    //Write your code here
+  	for(i=1;i<=n;i++)
+    {
+      for(j=1;j<=i;j++)
+      {
+        printf("*");
+      }
+      printf("\n");
+    }
 
   	return 0;
 }
